6ec808b2b491c620635353fe21579552c37f4c46
Richard Glew <richard.glew@gmail.com>
2014-01-07 23:58:46 +0000

    Updating solution with problem statement

--- 6.hs
+++ 6.hs
@@ -1,3 +1,11 @@
+-- The sum of the squares of the first ten natural numbers is,
+-- 12 + 22 + ... + 102 = 385
+-- The square of the sum of the first ten natural numbers is,
+-- (1 + 2 + ... + 10)2 = 552 = 3025
+-- Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 . 385 = 2640.
+-- Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
 --could be more terse but...
 --there are also formulas for these you could use
 --eg:sumOfSquares =  x(x+1)(2x+1)/6
