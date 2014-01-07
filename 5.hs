ffb974ab721549c14681d23ce1d830aede811cb9
Richard Glew <richard.glew@gmail.com>
2014-01-07 22:16:03 +0000

    this one made my brain hurt a bit

---
+++ 5.hs
@@ -0,0 +1,24 @@
+
+
+--2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+--What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+
+module Main where
+
+main :: IO()
+main =  print $ divisibleTo 20
+ 
+
+-- basic technique seems to be to divisible by X, x-1 ,x-2 etc...
+-- can be recursive
+-- start at 2520
+
+isDivisibleTo ::  Integer -> Integer -> Bool
+isDivisibleTo x n = all (\i -> n `mod` i == 0) (reverse [1..x])
+
+divisibleTo ::  Integer -> Integer
+divisibleTo 1 = 1
+divisibleTo x = let step = divisibleTo (x-1)
+                in  head $ filter (isDivisibleTo x) [step,2*step..]
+
