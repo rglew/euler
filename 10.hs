fcdbbe272afd283bbe370fe60575022c1d76c8e0
Richard Glew <richard.glew@gmail.com>
2014-01-10 02:03:39 +0000

    Used ONeill Primes - super fast

--- 10.hs
+++ 10.hs
@@ -2,6 +2,7 @@
 -- Find the sum of all the primes below two million.
 
 -- Probably supposed to code up a sieve of Erastothenes here but that's boring.  Am importin OneillPrimes which is awesome..nd exceedingly quick.
+-- http://www.haskell.org/haskellwiki/Prime_numbers
 
 module Main where 
 
