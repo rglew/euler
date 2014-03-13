c5549de122ab04fd756d6b1c8638f0362ef45ba9
richard glew <richard.glew@gmail.com>
2014-03-13 18:49:13 +1000

    23 & 24

--- 23.hs
+++ 23.hs
@@ -4,6 +4,23 @@ module Main where
 import Data.List
 import Data.Array
 
+data AbStatus = Abundant | Perfect | Deficient deriving (Show)
+
+
+main = do
+       let sumof2abundants = sum2 abundants abundants
+       print . sum $ filter (\x -> notElem x sumof2abundants) [16..28123]
+
+abundants = [x|x<-[12..28123],(d x) > x]
+
+sum2 x y = nub $ concat $ [map (+z) [a] | z<-x, a<-y]
+
+classify n
+
+  | d n > n = Abundant
+  | d n == n = Perfect
+  | d n < n = Deficient 
+
 d n = product [(p * product g - 1) `div` (p - 1) |
                  g <- group $ primeFactors n, let p = head g
               ] - n
