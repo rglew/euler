01f7f2c17938d3a02d24bf6505323e055c3da0f0
Richard Glew <richard.glew@gmail.com>
2014-04-25 10:47:45 +0000

    test

--- 1.hs
+++ 1.hs
@@ -3,7 +3,5 @@
 
 module One where
 
-problem_one = do
-
-     (sum [ x | x <- [1..1000], (x `mod` 3 == 0) || (x `mod` 5 == 0)])
+problem_one = (sum [ x | x <- [1..1000], (x `mod` 3 == 0) || (x `mod` 5 == 0)]
 
